948d0d3d67e546dab87915ec2f5c1ce12965bf77
SarahMubarak643 <sarnjssjah@gmail.com>
2026-08-19 11:11:16 +0300

    Create CourseEnrollmentCount.java

---
+++ src/main/java/courseEnrollement/example/demo/repository/CourseEnrollmentCount.java
@@ -0,0 +1,10 @@
+package courseEnrollement.example.demo.repository;
+
+public interface CourseEnrollmentCount {
+
+    String getCourseCode();
+
+    String getCourseName();
+
+    Long getEnrollmentCount();
+}
